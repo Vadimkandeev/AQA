fd9f2e615e9187358e9bbe8bb6dba95bbe3dc54e
Vadim <vadimkandeev@gmail.com>
2026-03-03 20:58:37 +0400

    Экзамен по Классам

--- module 3 python base/system_management.py
+++ module 3 python base/system_management.py
@@ -1,11 +1,80 @@
-class Task:
-    def __init__(self):
-        pass
+class DataError(Exception):
+    pass
 
 class Project:
+    def __init__(self, name_project):
+        self.tasks = []
+        self.name_project = name_project
+        self.tasks_in_progress = 0
+        self.tasks_done = 0
+
+    def get_progress(self):
+        status_list = ["New", "In progress", "Done"]
+        for task in self.tasks:
+            if task.status not in status_list:
+                raise DataError("Ошибка статуса")
+            elif task.status == "Done":
+                self.tasks_done += 1
+            else:
+                self.tasks_in_progress += 1
+        return self.name_project, self.tasks, self.tasks_in_progress, self.tasks_done
+
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+
+
+class Task:
     def __init__(self):
-        pass
+        self.status = "New"
+        self.assigned_to = None
+
+    def task_start(self):
+        if self.status is not "New":
+            raise DataError(f"Задача уже в статусе {self.status}")
+        self.status = "In progress"
+
+    def task_complete(self):
+        if self.status is not "In progress":
+            raise DataError(f"Задача в статусе {self.status}")
+        self.status = "Done"
+
+    def assign_to(self, developer):
+        if self.assigned_to is not None:
+            raise DataError(f"Задача уже назначена на {self.assigned_to.name}")
+        self.assigned_to = developer
+
+
 
 class Developer:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
+        self.complete_tasks = []
+
+    def take_task(self):
+
         pass
+
+    def complete_task(self, task):
+        if task not in self.tasks:
+            raise DataError("Ошибка назначения задачи")
+
+
+
+    def get_workload(self):
+        pass
+
+
+
+
+project = Project("My_notepad")
+
+desktop_task = Task()
+
+project.add_task(desktop_task)
+
+project.get_progress()
+
+
