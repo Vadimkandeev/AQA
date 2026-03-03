19855b4efd90d456578b96fe91108a2d2e1ba885
Вадим <ravenholm1@rambler.ru>
2026-03-03 23:46:50 +0400

    Обновление из дома

--- module 3 python base/system_management.py
+++ module 3 python base/system_management.py
@@ -5,19 +5,20 @@ class Project:
     def __init__(self, name_project):
         self.tasks = []
         self.name_project = name_project
-        self.tasks_in_progress = 0
-        self.tasks_done = 0
+
 
     def get_progress(self):
         status_list = ["New", "In progress", "Done"]
+        tasks_not_done = 0
+        tasks_done = 0
         for task in self.tasks:
             if task.status not in status_list:
                 raise DataError("Ошибка статуса")
             elif task.status == "Done":
-                self.tasks_done += 1
+                tasks_done += 1
             else:
-                self.tasks_in_progress += 1
-        return self.name_project, self.tasks, self.tasks_in_progress, self.tasks_done
+                tasks_not_done += 1
+        return self.name_project, self.tasks, tasks_not_done, tasks_done
 
 
     def add_task(self, task):
@@ -31,19 +32,22 @@ class Task:
         self.assigned_to = None
 
     def task_start(self):
-        if self.status is not "New":
+        if self.status != "New":
             raise DataError(f"Задача уже в статусе {self.status}")
         self.status = "In progress"
 
+
     def task_complete(self):
-        if self.status is not "In progress":
+        if self.status != "In progress":
             raise DataError(f"Задача в статусе {self.status}")
         self.status = "Done"
+
 
     def assign_to(self, developer):
         if self.assigned_to is not None:
             raise DataError(f"Задача уже назначена на {self.assigned_to.name}")
         self.assigned_to = developer
+
 
 
 
@@ -53,18 +57,23 @@ class Developer:
         self.tasks = []
         self.complete_tasks = []
 
-    def take_task(self):
+    def take_task(self, task):
+        task.assign_to(self)
+        task.task_start()
+        self.tasks.append(task)
 
-        pass
 
     def complete_task(self, task):
         if task not in self.tasks:
             raise DataError("Ошибка назначения задачи")
-
+        task.task_complete()
+        self.tasks.remove(task)
+        self.complete_tasks.append(task)
 
 
     def get_workload(self):
-        pass
+        return self.tasks, self.complete_tasks
+
 
 
 
