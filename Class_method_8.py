e6f85785f5e4fe7ecc1721619538f09079e7bf7f
Вадим <ravenholm1@rambler.ru>
2026-03-18 23:44:30 +0400

    Обновление из дома

--- Class_method_8.py
+++ Class_method_8.py
@@ -4,4 +4,19 @@ class Temperature:
 
     @property
     def celsius(self):
-        pass
+        return self._celsius
+
+
+    @celsius.setter
+    def celsius(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("Температура должна быть числом")
+        if value < -273.15:
+            raise ValueError("Температура не может быть ниже абсолютного нуля")
+        self._celsius = value
+
+
+
+temp = Temperature(1500.5)
+
+print(temp.celsius)
---
+++ Class_method_9.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
