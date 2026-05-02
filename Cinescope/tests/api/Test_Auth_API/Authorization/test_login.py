97944b331f9685a23b556f00d53e5adb1c234ec5
Вадим <ravenholm1@rambler.ru>
2026-05-03 00:59:46 +0400

    Работа из дома

--- Cinescope/tests/api/Test_Auth_API/Authorization/test_login.py
+++ Cinescope/tests/api/Test_Auth_API/Authorization/test_login.py
@@ -2,28 +2,86 @@ import pytest
 import requests
 from constants import BASE_URL, HEADERS, LOGIN_ENDPOINT
 
-# Аутентификация пользователя
+# Аутентификация пользователя.
 class TestAuth:
-        def test_authentication_user(self, created_user_by_user, random_user_by_user):
-            # URL для аутентификации
-            authentication_url = f"{BASE_URL}{LOGIN_ENDPOINT}"
-            body = {"email": random_user_by_user["email"], "password": random_user_by_user["password"]}
+     # Аутентификация пользователя. Валидный запрос.
+    def test_authentication_user(self, random_user_by_user):
+        # URL для аутентификации
+        authentication_url = f"{BASE_URL}{LOGIN_ENDPOINT}"
+        body = {"email": random_user_by_user["email"], "password": random_user_by_user["password"]}
 
-            # Отправка запроса для аутентификации
-            response = requests.post(authentication_url, json=body, headers=HEADERS)
+        # Отправка запроса для аутентификации
+        response = requests.post(authentication_url, json=body, headers=HEADERS)
 
-            # Логируем ответ для диагностики
-            print(f"Response status: {response.status_code}")
-            print(f"Response body: {response.text}")
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
 
-            # Проверки
-            assert response.status_code == 200, "Ошибка аутентификации пользователя"
-            response_data = response.json()
-            assert response_data["user"]["email"] == random_user_by_user["email"], "Email не совпадает"
-            assert "id" in response_data["user"], "ID пользователя отсутствует в ответе"
-            assert "roles" in response_data["user"], "Роли отсутствуют в ответе"
-            assert "USER" in response_data["user"]["roles"], "Роль пользователя отсутствует в ответе"
-            assert "accessToken" in response_data, "accessToken отсутствует в ответе"
-            assert response_data["accessToken"], "Пустое значение accessToken"
-            assert "refreshToken" in response_data, "refreshToken отсутствует в ответе"
-            assert response_data["refreshToken"], "Пустое значение refreshToken"
+        # Проверки
+        assert response.status_code == 200, "Ошибка аутентификации пользователя"
+        response_data = response.json()
+        assert response_data["user"]["email"] == random_user_by_user["email"], "Email не совпадает"
+        assert "id" in response_data["user"], "ID пользователя отсутствует в ответе"
+        assert "roles" in response_data["user"], "Роли отсутствуют в ответе"
+        assert "USER" in response_data["user"]["roles"], "Роль пользователя отсутствует в ответе"
+        assert "accessToken" in response_data, "accessToken отсутствует в ответе"
+        assert response_data["accessToken"], "Пустое значение accessToken"
+        assert "refreshToken" in response_data, "refreshToken отсутствует в ответе"
+        assert response_data["refreshToken"], "Пустое значение refreshToken"
+
+
+    # Аутентификация пользователя. Невалидный email.
+    def test_negative_authentication_user_invalid_email(self, random_user_by_user):
+        # URL для аутентификации
+        authentication_url = f"{BASE_URL}{LOGIN_ENDPOINT}"
+
+        invalid_email = "invalid_email@mail.ru"
+        body = {"email": invalid_email, "password": random_user_by_user["password"]}
+
+        # Отправка запроса для аутентификации
+        response = requests.post(authentication_url, json=body, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 401, "Ошибка валидации с невалидным email"
+
+
+    # Аутентификация пользователя. Невалидный email.
+    def test_negative_authentication_user_invalid_pass(self, random_user_by_user):
+        # URL для аутентификации
+        authentication_url = f"{BASE_URL}{LOGIN_ENDPOINT}"
+
+        invalid_password = "invalid_password"
+        body = {"email": random_user_by_user["email"], "password": invalid_password}
+
+        # Отправка запроса для аутентификации
+        response = requests.post(authentication_url, json=body, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 401, "Ошибка валидации с невалидным паролем"
+
+
+
+        # Аутентификация пользователя. Пустое тело запроса.
+    def test_negative_authentication_user_empty_body(self, random_user_by_user):
+        # URL для аутентификации
+        authentication_url = f"{BASE_URL}{LOGIN_ENDPOINT}"
+
+        body = {}
+
+        # Отправка запроса для аутентификации
+        response = requests.post(authentication_url, json=body, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 401, "Ошибка валидации с пустым телом запроса"
--- Cinescope/tests/api/Test_Auth_API/Authorization/test_register.py
+++ Cinescope/tests/api/Test_Auth_API/Authorization/test_register.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 from constants import BASE_URL, HEADERS, REGISTER_ENDPOINT, CONFIRM_ENDPOINT, LOGIN_ENDPOINT, LOGOUT_ENDPOINT,REFRESH_TOKENS_ENDPOINT
 
-# Регистрация нового пользователя
+# Регистрация нового пользователя позитивная проверка
 class TestRegistration:
     def test_register_user(self, random_user_by_user):
         # URL для регистрации
@@ -26,3 +26,121 @@ class TestRegistration:
         assert "USER" in response_data["roles"], "Роль USER должна быть у пользователя"
 
 
+    # Регистрация нового пользователя без поля подтверждения пароля
+    def test_negative_register_without_confirm_pass(self, random_user_by_user):
+        # URL для регистрации
+        register_url = f"{BASE_URL}{REGISTER_ENDPOINT}"
+
+        # Фабрикуем невалидное тело запроса (удаляем поле подтверждение пароля)
+        body = random_user_by_user.copy()
+        del body["passwordRepeat"]
+
+        # Отправка запроса на регистрацию
+        response = requests.post(register_url, json=body, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 400, "Ошибка валидации при отсутствии подтверждения пароля"
+
+    # Регистрация нового пользователя без поля ФИО
+    def test_negative_register_without_fullname(self, random_user_by_user):
+        # URL для регистрации
+        register_url = f"{BASE_URL}{REGISTER_ENDPOINT}"
+
+        # Фабрикуем невалидное тело запроса (удаляем поле ФИО)
+        body = random_user_by_user.copy()
+        del body["fullName"]
+
+        # Отправка запроса на регистрацию
+        response = requests.post(register_url, json=body, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 400, "Ошибка валидации при отсутствии поля ФИО"
+
+
+    # Регистрация нового пользователя без поля email
+    def test_negative_register_without_email(self, random_user_by_user):
+        # URL для регистрации
+        register_url = f"{BASE_URL}{REGISTER_ENDPOINT}"
+
+        # Фабрикуем невалидное тело запроса (удаляем поле email)
+        body = random_user_by_user.copy()
+        del body["email"]
+
+        # Отправка запроса на регистрацию
+        response = requests.post(register_url, json=body, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 400, "Ошибка валидации при отсутствии поля email"
+
+
+    # Регистрация нового пользователя с несовпадающими паролями
+    def test_negative_register_password_mismatch(self, random_user_by_user):
+        # URL для регистрации
+        register_url = f"{BASE_URL}{REGISTER_ENDPOINT}"
+
+        # Фабрикуем невалидное тело запроса (изменяем повторный пароль)
+        body = random_user_by_user.copy()
+        body["passwordRepeat"] = "OtherPass12345678"
+
+        # Отправка запроса на регистрацию
+        response = requests.post(register_url, json=body, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 400, "Ошибка валидации при несовпадении 'passwordRepeat'"
+
+
+        # Регистрация нового пользователя с невалидным email
+    def test_negative_register_invalid_email(self, random_user_by_user):
+        # URL для регистрации
+        register_url = f"{BASE_URL}{REGISTER_ENDPOINT}"
+
+        # Фабрикуем невалидное тело запроса (делаем невалидный email)
+        body = random_user_by_user.copy()
+        body["email"] = body["email"].replace("@", "F")
+
+        # Отправка запроса на регистрацию
+        response = requests.post(register_url, json=body, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 400, "Ошибка валидации при невалидном поле 'email'"
+
+
+        # Регистрация нового пользователя со слишком коротким паролем
+    def test_negative_register_short_email(self, random_user_by_user):
+        # URL для регистрации
+        register_url = f"{BASE_URL}{REGISTER_ENDPOINT}"
+
+        # Фабрикуем невалидное тело запроса (создаем короткий пароль)
+        body = random_user_by_user.copy()
+        body["password"] = "Q1"
+        body["passwordRepeat"] = "Q1"
+
+        # Отправка запроса на регистрацию
+        response = requests.post(register_url, json=body, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 400, "Ошибка валидации при слишком коротком пароле"
