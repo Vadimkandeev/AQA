732bd6dc40c545a26e01b10ce5a5cb1e851c6e9c
Vadim <vadimkandeev@gmail.com>
2026-05-18 21:09:28 +0400

    Изменения с работы

--- Cinescope/tests/api/args.py
+++ Cinescope/tests/api/args.py
@@ -24,17 +24,50 @@
 #
 # print(filter_kwargs(a=5, b=20, c=15, d=3))
 
-default_settings = {"theme": "light", "notifications": True}
+#default_settings = {"theme": "light", "notifications": True}
 
 def log_kwargs(func):
-    def wrapper():
-        func()
-        return {}
+    def wrapper(a, b, **kwargs):
+        res = func(a, b, **kwargs)
+        return res
+    return wrapper
 
-@log_kwargs
-def my_function(a, b, **kwargs):
-    return a+b
+# @log_kwargs
+# def my_function(a, b, **kwargs):
+#     return kwargs
+#
+# print(my_function(5, 10, theme="light", notifications=True))
 
-my_function(5, 10, **default_settings)
+# def my_decor(func):
+#     def wrapper(*args, **kwargs):
+#         print("function is run")
+#         return func(*args, **kwargs)
+#     return wrapper
+#
+#
+# @my_decor
+# def my_function(a, b):
+#     return a + b
+#
+# print(my_function(5, 10))
+
+
+def my_decor(func):
+    def wrapper(*args, **kwargs):
+        print("START")
+        result = func(*args, **kwargs)
+        print(result)
+        print("FINISH")
+        return result
+
+    return wrapper
+
+@my_decor
+def my_func(a, b, c):
+    return a+b+c
+
+
+my_func(2,6,999)
+
 
 
