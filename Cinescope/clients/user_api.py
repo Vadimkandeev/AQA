430820dc7f157527d7fd0e5afa53ab4c92beac0d
Вадим <ravenholm1@rambler.ru>
2026-06-22 21:38:30 +0400

    Изменения из дома.

--- Cinescope/clients/user_api.py
+++ Cinescope/clients/user_api.py
@@ -1,5 +1,5 @@
 from custom_requester.custom_requester import CustomRequester
-from constants import REGISTER_ENDPOINT, LOGIN_ENDPOINT, BASE_API_URL
+from constants import REGISTER_ENDPOINT, LOGIN_ENDPOINT, BASE_API_URL, USER_ENDPOINT
 
 
 class UserApi(CustomRequester):
@@ -11,3 +11,16 @@ class UserApi(CustomRequester):
         super().__init__(session=session, base_url=BASE_API_URL)
         self.session = session
 
+
+    def get_user_info(self, user_id, expected_status=200):
+        """
+        ПОлучение информации о пользователе.
+        :param user_id: АйДи пользователя
+        :param expected_status: Ожидаемый статус-код
+        """
+        return self.send_request(
+            method="GET",
+            endpoint=f"{USER_ENDPOINT}/{user_id}"
+        )
+
+    def delete_user(self, user_id, expected_status=204):
