1a4a085de644443491655b90d642d56f9c56f498
Vadim <vadimkandeev@gmail.com>
2026-07-10 21:07:06 +0400

    Изменения с работы

--- Cinescope/tests/api_client/test_auth.py
+++ Cinescope/tests/api_client/test_auth.py
@@ -5,12 +5,12 @@ import pytest
 from utils.data_generator import DataGenerator
 from random import randint
 from api.api_manager import ApiManager
-
+from utils.data_generator import DataGenerator
 
 
 class TestAuth:
 
-    def test_auth(self, api_manager, random_user_by_user):
+    def test_register_new_user(self, api_manager, random_user_by_user):
         response = api_manager.auth_api.register_user(random_user_by_user)
         response_data = response.json()
 
@@ -18,4 +18,23 @@ class TestAuth:
         assert response_data["email"] == random_user_by_user["email"], "Email не совпадает"
         assert "id" in response_data, "ID пользователя отсутствует в ответе"
         assert "USER" in response_data["roles"], "Роль USER должна быть у пользователя"
+
+
+
+    @pytest.mark.parametrize("missing_field", ["email", "fullName", "password", "passwordRepeat"])
+    def test_negative_register_user(self, api_manager, random_user_by_user, missing_field):
+        body = random_user_by_user.copy()
+        del body[missing_field]
+
+        api_manager.auth_api.register_user(body, expected_status=400)
+
+    def test_password_repeat(self, api_manager, random_user_by_user, generate_random_password):
+        body = random_user_by_user.copy()
+        body["passwordRepeat"] = generate_random_password
+        api_manager.auth_api.register_user(body, expected_status=400)
+
+
+
+
+
 
